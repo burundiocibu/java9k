4c47d842b8cbed4ecb2d3a0f23f6d2bef8d118c8
Jon Little <burundiocibu@gmail.com>
2016-03-24 21:44:09 -0500

    Rewrote into a class that should work better in a polling situation

--- weigand.py
+++ weigand.py
@@ -1,94 +1,89 @@
 #!/usr/bin/env python
-""" Module to read a raspberry Pi's GPIO lines #18 & #23
- as a Weigand interface from a ID card reader"""
 
 import argparse
 import RPi.GPIO as gpio
 import time
 from numpy import array
 
-gpio.setmode(gpio.BCM)
 
-D0 = 23
-D1 = 24
-gpio.setup(D0, gpio.IN) # D0
-gpio.setup(D1, gpio.IN) # D1
+class Weigand:
+    """ Class to read a raspberry Pi's GPIO lines #18 & #23
+    as a Weigand interface from a ID card reader"""
+    def __init__(self, debug=False):
+        self.buff = []
+        self.trx = []
+        self.debug = debug
+        self.bit_time = 1e-3 # seconds per bit
 
-buff=[]
-trx=[]
+        gpio.setmode(gpio.BCM)
+        self.D0 = 23
+        self.D1 = 24
+        gpio.setup(self.D0, gpio.IN) # D0
+        gpio.setup(self.D1, gpio.IN) # D1
+        gpio.add_event_detect(self.D0, gpio.FALLING)
+        gpio.add_event_detect(self.D1, gpio.FALLING)
+        gpio.add_event_callback(self.D0, self.d0_callback)
+        gpio.add_event_callback(self.D1, self.d1_callback)
 
-def d0_callback(chan):
-    """ Called when a zero bit is received"""
-    global buff,trx
-    buff.append(0)
-    trx.append(time.time())
 
-def d1_callback(chan):
-    """ Called when a one bit is received"""
-    global buff,trx
-    buff.append(1)
-    trx.append(time.time())
+    def d0_callback(self, channel):
+        """ Called when a zero bit is received"""
+        self.remove_previous()
+        self.buff.append(0)
+        self.trx.append(time.time())
 
-gpio.add_event_detect(D0, gpio.FALLING)
-gpio.add_event_detect(D1, gpio.FALLING)
-gpio.add_event_callback(D0, d0_callback)
-gpio.add_event_callback(D1, d1_callback)
+    def d1_callback(self, channel):
+        """ Called when a one bit is received"""
+        self.remove_previous()
+        self.buff.append(1)
+        self.trx.append(time.time())
 
-def get_id(num_bits=40, debug=False):
-    """ Returns (facory_code,card_id) decoded from a card reader. The older HID
-    readers seem to have 40 bit cards while the newer ones seem to be 24."""
-    global buff,trx
-    buff=[]
-    trx=[]
-    while len(buff) < num_bits:
-        time.sleep(0.02)
-        if len(trx) > 0:
-            dt = time.time() - trx[-1]
-            if dt > 0.05:
-                if (debug):
-                    print "timeout"
-                break
-    if (debug):
-        trx = array(trx)
-        dtrx = trx[1:]-trx[0:-1]
-        print "buff:", len(buff), buff
-        print "mean dt={:.3} ms".format(1000*dtrx.mean())
+    def remove_previous(self):
+        if len(self.trx) > 0 and time.time() - self.trx[-1] > 5*self.bit_time:
+            if self.debug:
+                print "Timeout from reader."
+            self.buff = []
+            self.trx = []
 
-    fc=0
-    id=0
-    if len(buff) == num_bits:
-        d=array(buff)
-        pe=sum(d[1:13]) & 0x1
-        po=sum(d[13:-1]) & 0x1
+    def get_id(self):
+        id = 0L
+        if len(self.trx) == 0 or time.time() - self.trx[-1] < 5*self.bit_time:
+            return id
+        if self.debug:
+            trx = array(self.trx)
+            dtrx = trx[1:]-trx[0:-1]
+            print "buff:", len(self.buff), self.buff
+            print "mean dt={:.3} ms".format(1000*dtrx.mean())
+        d = array(self.buff)
+        pe = sum(d[1:13]) & 0x1
+        po = sum(d[13:-1]) & 0x1
         if pe == d[0] and po != d[-1]:
-            d_fc = d[1:9]
-            d_id = d[9:-2]
-
-            fc=d_fc[0];
-            for b in d_fc[1:]:
-                fc <<= 1
-                fc |= b
-            id=d_id[0]
-            for b in d_id[1:]:
+            id = d[0] + 0L
+            for b in d[1:]:
                 id <<= 1
                 id |= b
         else:
-            if (debug):
+            if self.debug:
                 print "Parity error"
+        self.buff = []
+        self.trx = []
+        return id
 
-    return (fc, id)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tests reading id card data.")
     parser.add_argument("-d", "--debug", dest='debug', action='count',
                         help="Increase debug output")
-    parser.add_argument("-b", "--bits", dest='bits', default=40,
-                         help="Number of bits expected in a card")
     args = parser.parse_args()
-    
+
+    stinkin = Weigand(debug=args.debug>0)
+
     try:
         while True:
-            (fc,id) = get_id(num_bits=args.bits, debug=args.debug>0)
-            print "fc:{:x} id:{:x}".format(fc, id)
+            id = stinkin.get_id()
+            if id != 0:
+                print "id:{:x}".format(id)
+            else:
+                time.sleep(0.1)
     except KeyboardInterrupt:
         gpio.cleanup()       # clean up GPIO on CTRL+C exit
